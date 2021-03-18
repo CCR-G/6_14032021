74935064fa2ad99adb4b53a1fbc8cd6dbb912699
CCR-G <camillecroquet@gmail.com>
2021-03-18 19:05:02 +0100

    Adding modify and delete routes
    This patch introduces the logic for the Delete and Modify routes.

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,4 +1,5 @@
 const Sauce = require('../models/sauce');
+const fs = require('fs');
 
 exports.create = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
@@ -19,12 +20,33 @@ exports.getOne = (req, res, next) => {
 };
 
 exports.modify = (req, res, next) => {
+    const sauceObject = req.file ?
+        {
+            ...JSON.parse(req.body.sauce),
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        } : { ...req.body };
+    Sauce.updateOne(
+        { _id: req.params.id },
+        { ...sauceObject, _id: req.params.id }
+    )
+        .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
+        .catch(error => res.status(400).json({ error }));
 };
 
 exports.like = (req, res, next) => {
 };
 
 exports.delete = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            const filename = sauce.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`, () => {
+                Sauce.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
+                    .catch(error => res.status(400).json({ error }));
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
 exports.getAll = (req, res, next) => {
