7fa12a2c23683bb61a6cee952e965db489d411a7
CCR-G <camillecroquet@gmail.com>
2021-03-26 11:07:28 +0100

    Refactoring user controller
    Extracting code parts for clarity and code readability.
    It avoids "callback hell" extracting one `then` from the main function.

--- controllers/user.js
+++ controllers/user.js
@@ -3,12 +3,8 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res) => {
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
-                email: req.body.email,
-                password: hash
-            });
+    createSecureUser(req.body.email, req.body.password)
+        .then(user => {
             user.save()
                 .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
                 .catch(error => res.status(400).json({ error }));
@@ -16,27 +12,41 @@ exports.signup = (req, res) => {
         .catch(error => res.status(500).json({ error }));
 };
 
+async function createSecureUser(email, password, salt = 10) {
+    const hashed_password = await bcrypt.hash(password, salt);
+    return new User({
+        email: email,
+        password: hashed_password
+    });
+}
+
 exports.login = (req, res) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
                 return res.status(401).json({ error: 'Utilisateur non trouvé !' });
             }
+
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
                         return res.status(401).json({ error: 'Mot de passe incorrect !' });
                     }
-                    res.status(200).json({
-                        userId: user._id,
-                        token: jwt.sign(
-                            { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '1h' }
-                        )
-                    });
+                    const token = getToken(user);
+                    res.status(200).json(token);
                 })
                 .catch(error => res.status(500).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
 };
+
+function getToken(user) {
+    return {
+        userId: user._id,
+        token: jwt.sign(
+            { userId: user._id },
+            'RANDOM_TOKEN_SECRET',
+            { expiresIn: '1h' }
+        )
+    }
+}
