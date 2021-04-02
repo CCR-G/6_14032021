cf6229c9aa7a447591062cdfc7f5d1885495e2ca
CCR-G <camillecroquet@gmail.com>
2021-04-02 09:34:10 +0200

    Adding rate limiter
    Package express-rate-limit allows to limit requests made to the API.
    Here it is use to prevent too many login attempts.

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const rateLimit = require("express-rate-limit");
 const router = express.Router();
 
 const userCtrl = require('../controllers/user');
 const passwordVerification = require('../middleware/password-verification');
 
 router.post('/signup', passwordVerification, userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login', rateLimit({ max: 3 }), userCtrl.login);
 
 module.exports = router;
